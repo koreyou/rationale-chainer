2af37df79e7a26557e8e9d7b47c5cf426785a982
Yuta Koreeda <koreyou@mac.com>
2018-03-29 23:21:14 +0900

    Renamed classifier (since it is not a classifier)

--- rationale/models/__init__.py
+++ rationale/models/__init__.py
@@ -1,4 +1,4 @@
 
-from rationale.models.classifier import RationalizedRegressor
+from rationale.models.rationalized_regressor import RationalizedRegressor
 from rationale.models.encoder import LSTMEncoder
 from rationale.models.generator import LSTMGenerator
